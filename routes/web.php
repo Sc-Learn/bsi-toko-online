055ec6e1361a730f0fc6dd3425ab999b89a5bcc4
zidane-sc <zidankren02@gmail.com>
2025-05-10 17:43:55 +0700

    feat(minggu-6): Manajemen keranjang belanja

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProdukController;
 
 
@@ -78,4 +79,8 @@ Route::middleware('is.customer')->group(function () {
     Route::get('/customer/akun/{id}', [CustomerController::class, 'akun'])->name('customer.akun');
     // Route untuk mengupdate data akun customer
     Route::put('/customer/updateakun/{id}', [CustomerController::class, 'updateAkun'])->name('customer.updateakun');
+
+    // Route untuk menambahkan produk ke keranjang
+    Route::post('add-to-cart/{id}', [OrderController::class, 'addToCart'])->name('order.addToCart');
+    Route::get('cart', [OrderController::class, 'viewCart'])->name('order.cart');
 });
